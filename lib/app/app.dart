aa3eaee196859387e3df501a1cc222ed5b65ec73
Aryan-Bhargav8 <bhargav.aryan8@gmail.com>
2024-02-23 14:05:48 +0530

    First Commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/home_view.dart';
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      home: HomeView(),
+    );
+  }
+}
+
